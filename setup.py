a5f1f4cba80e84874aa020b1bb4fa00e526cbd89
willnx <willnx84@gmail.com>
2019-06-24 12:16:30 -0700

    Updated to support vlab-inf-common==2019.06.24

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(name="vlab-inventory-api",
       author="Nicholas Willhite,",
       author_email='willnx84@gmail.com',
-      version='2019.06.18',
+      version='2019.06.24',
       packages=find_packages(),
       include_package_data=True,
       package_files={'vlab_inventory_api' : ['app.ini']},
